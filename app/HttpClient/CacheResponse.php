6109b95d75d4c2ed2330d14c0159d573431c134e
MilesChou <jangconan@gmail.com>
2021-10-10 23:12:58 +0800

    Add cache

---
+++ app/HttpClient/CacheResponse.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\HttpClient;
+
+use Closure;
+use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Psr7\HttpFactory;
+use GuzzleHttp\Psr7\Utils;
+use Illuminate\Support\Facades\Cache;
+use Psr\Http\Message\RequestInterface;
+
+class CacheResponse
+{
+    /**
+     * @var null
+     */
+    private $ttl;
+
+    public function __construct($ttl = null)
+    {
+        $this->ttl = $ttl;
+    }
+
+    public function __invoke(callable $handler): Closure
+    {
+        return function (RequestInterface $request, array $options) use ($handler) {
+            $key = sha1(json_encode([
+                $request->getMethod(),
+                (string)$request->getUri(),
+                (string)$request->getBody(),
+                $request->getHeaders(),
+            ]));
+
+            if ($data = Cache::store('file')->get($key)) {
+                $response = (new HttpFactory())->createResponse($data['statusCode'])
+                    ->withBody(Utils::streamFor($data['body']));
+
+                foreach ($data['headers'] as $key => $value) {
+                    $response = $response->withHeader($key, $value);
+                }
+
+                return new FulfilledPromise($response);
+            }
+
+            /** @var FulfilledPromise $promise */
+            $promise = $handler($request, $options);
+
+            $response = $promise->wait();
+
+            Cache::store('file')->put($key, [
+                'statusCode' => $response->getStatusCode(),
+                'headers' => $response->getHeaders(),
+                'body' => (string)$response->getBody(),
+            ], $this->ttl);
+
+            return $promise;
+        };
+    }
+}
--- app/Providers/ClientServiceProvider.php
+++ app/Providers/ClientServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\HttpClient\CacheResponse;
 use Illuminate\Http\Client\Factory;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
@@ -15,7 +16,7 @@ class ClientServiceProvider extends ServiceProvider
 
             // 因呼叫 twse 的 API 太頻繁的話會 ban IP，所以這裡做了硬幹阻擋的小程式
             // 目前有看到網路有分享規則是 5 秒內不能超過 3 次，這裡設定是 5 秒內可以連續呼叫 2 次，第三次開始會等 1 秒。
-            return (new Factory())->beforeSending(function () use ($cache) {
+            $pendingRequest = (new Factory())->beforeSending(function () use ($cache) {
                 while ($cache->get('lock') >= 2) {
                     sleep(1);
                 };
@@ -26,6 +27,11 @@ class ClientServiceProvider extends ServiceProvider
                     $cache->put('lock', 1, 5);
                 }
             });
+
+            // 加快取
+            $pendingRequest = $pendingRequest->withMiddleware(new CacheResponse());
+
+            return $pendingRequest;
         });
     }
 }
