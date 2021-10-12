811794aedbd04dc10644088aa73ee2cee1433083
miles.chou <miles.chou@104.com.tw>
2021-10-12 14:09:07 +0800

    Add chart for sre

--- app/Commands/Sre.php
+++ app/Commands/Sre.php
@@ -3,17 +3,22 @@
 namespace App\Commands;
 
 use App\Crawler\OverviewCrawler;
+use App\HttpClient\ClientFactory;
 use Carbon\Carbon;
+use GuzzleHttp\Psr7\HttpFactory;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
 
 class Sre extends Command
 {
-    protected $signature = 'sre {day?} {month?} {year?}';
+    protected $signature = 'sre {day?} {month?} {year?}
+                                {--take=10}
+                                {--graph}';
 
     protected $description = '查詢歷年股利';
 
-    public function handle(OverviewCrawler $overview): int
+    public function handle(OverviewCrawler $overview, ClientFactory $client): int
     {
         $day = $this->argument('day');
         $month = $this->argument('month');
@@ -44,7 +49,7 @@ class Sre extends Command
                 return $value;
             })
             ->sortBy(4)
-            ->toArray();
+            ->take($this->option('take'));
 
         $this->table([
             '證券代號',
@@ -55,7 +60,35 @@ class Sre extends Command
             '股價淨值比',
             '財報年/季',
             '評價',
-        ], $data);
+        ], $data->toArray());
+
+        if ($this->option('graph')) {
+            // See http://coopermaa2nd.blogspot.com/2011/01/google-chart-api.html
+            $parameter = [
+                'cht' => 'bvg',
+                'chs' => '700x400',
+                'chd' => 't:' . $data->map(fn($v) => 100 * (1 / $v[4]))->implode(','),
+                'chxl' => '0:|' . $data->map(fn($v) => $v[1])->implode('|'),
+                'chxt' => 'x',
+                'chxr' => '1,0,1',
+                'chg' => '10,10',
+            ];
+
+            $uri = (new HttpFactory())
+                ->createUri('https://chart.googleapis.com/chart')
+                ->withQuery(Arr::query($parameter));
+
+            $b = $client->get($uri)->body();
+
+            $this->newLine();
+
+            // See https://iterm2.com/documentation-images.html
+            $image = "\033]1337;File=inline=1;width=80%:" . base64_encode($b) . "\007";
+
+            $this->line($image);
+
+            return 0;
+        }
 
         return 0;
     }
