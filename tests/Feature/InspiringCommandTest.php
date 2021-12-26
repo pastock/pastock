4c9b3f788ed42afa6851ff62e1294e05ab81f154
MilesChou <jangconan@gmail.com>
2021-12-26 23:39:26 +0800

    Remove unused command

--- tests/Feature/InspiringCommandTest.php
+++ tests/Feature/InspiringCommandTest.php
@@ -1,7 +1,1 @@
 <?php
-
-test('inspiring command', function () {
-    $this->artisan('inspiring')
-         ->expectsOutput('Simplicity is the ultimate sophistication.')
-         ->assertExitCode(0);
-});
